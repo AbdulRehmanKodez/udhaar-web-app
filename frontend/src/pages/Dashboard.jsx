f87d904ac3a776a9a95710e404d9d8cfaffd6c81
AbdulRehmanKodez <abdulrehmanbutt.to@gmail.com>
2026-03-19 23:38:30 +0500

    meow

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -21,14 +21,30 @@ function Dashboard() {
 
   return (
     <>
-      <div style={{paddingLeft:'10px'}} className='h-20 w-full  text-black text-3xl font-bold flex items-center '>
-        <h1>UDHAR KHATA</h1>
+      <div style={{paddingLeft:'10px'}} className='h-20 w-full  text-black gap-5 text-3xl font-bold flex items-center '>
+        <h1 className=''>UDHAR KHATA</h1>
+
+
+         <div style={{marginLeft:'120px'}} className='ml-20'>
+          <input
+           
+           style={{height:'35px',fontSize:'20px',borderRadius:'5px',paddingLeft:'2px'}}
+           className='border-2 border-gray-950 outline-none'
+            type="text"
+            placeholder="Search by name or phone"
+            />
+         </div>
+
         <button
           onClick={() => setShowAddCard(true)} style={{marginLeft:'100px'}}  
-          className='bg-[#AEC3B0] text-[#01161E] h-8 w-45  font-bold py-2 px-4 rounded hover:bg-[#8AA695] text-base cursor-pointer transition-all'
+          className='bg-[#AEC3B0] text-[#ffffff] h-8 w-45  font-bold py-2 px-4 rounded hover:bg-[#8AA695] text-base cursor-pointer transition-all'
         >
           Add Customer
         </button>
+      </div>
+
+      <div className='h-0.5 bg-black'>
+
       </div>
 
      
@@ -49,10 +65,10 @@ function Dashboard() {
                 key={cust._id}
                 onClick={() => navigate(`/customer/${cust._id}`)} style={{paddingLeft:'10px',paddingTop:'8px'}} 
                 className='cursor-pointer h-24 w-70 leading-4.5   bg-[#598392] rounded-xl hover:bg-[#124559] transition-all shadow-md'
-            >      <h1>Name</h1>
-                <h1  className='text-xl font-bold text-white pl-20 '>{cust.name}</h1>
-                 <h2>Phone</h2>
-                <p className='text-white'>{cust.phone}</p>
+            >      <p style={{fontSize:'12px', color:'#d1d5db'}}>Name</p>
+    <p style={{fontSize:'18px', fontWeight:'bold', color:'white'}}>{cust.name}</p>
+    <p style={{fontSize:'12px', color:'#d1d5db', marginTop:'6px'}}>Phone</p>
+    <p style={{color:'white'}}>{cust.phone}</p>
             </div>
         ))}
     </div>
